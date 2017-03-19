bc0109b23ddea768390c684e99314c1582fdba66
Wilson.ST <wilsonthehungryman@gmail.com>
2017-03-19 17:25:23 -0500

    For push for use on other machine

--- app/src/main/java/com/rrc/wilson/developerreference/MainMenuActivity.java
+++ app/src/main/java/com/rrc/wilson/developerreference/MainMenuActivity.java
@@ -1,7 +1,9 @@
 package com.rrc.wilson.developerreference;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        View sView = findViewById(R.id.scrollView);
+        int height = sView.getHeight();
+        LinearLayout layout = (LinearLayout)findViewById(R.id.menuLayout);
+        layout.getLayoutParams().height = height;
+        layout.requestLayout();
+
+        //iter
+
     }
 }
