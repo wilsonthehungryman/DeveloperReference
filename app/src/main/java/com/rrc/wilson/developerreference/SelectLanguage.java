60a071b0115bfd29822de74d636948dedfb60f92
Wilson.ST <wilsonthehungryman@gmail.com>
2017-04-02 13:19:25 -0500

    Added search functionality to SelectLanguage activity.

--- app/src/main/java/com/rrc/wilson/developerreference/SelectLanguage.java
+++ app/src/main/java/com/rrc/wilson/developerreference/SelectLanguage.java
@@ -3,18 +3,22 @@ package com.rrc.wilson.developerreference;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SearchView;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
-public class SelectLanguage extends AppCompatActivity {
+public class SelectLanguage extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     LinearLayout mLayout;
     ArrayList<LanguageDescription> languages;
     DatabaseHelper dbHelper;
+    SearchView search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +32,15 @@ public class SelectLanguage extends AppCompatActivity {
         else
             languages = dbHelper.getSupportedLanguages();
         dbHelper = null;
-        mLayout = (LinearLayout) findViewById(R.id.layout);
+
+        mLayout = (LinearLayout) findViewById(R.id.items);
+        search = (SearchView)findViewById(R.id.search);
+
+        try {
+            search.setOnQueryTextListener(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         if(intent.getIntExtra("source", 0) == R.id.searchOfficial) {
             generateCheckboxes();
@@ -45,7 +57,7 @@ public class SelectLanguage extends AppCompatActivity {
             CheckBox chkBox = new CheckBox(this);
             chkBox.setChecked(false);
             chkBox.setText(lang.getName());
-            mLayout.addView(chkBox, mLayout.getChildCount() - 1);
+            mLayout.addView(chkBox);
         }
     }
 
@@ -57,7 +69,25 @@ public class SelectLanguage extends AppCompatActivity {
             TextView textView = new TextView(this);
             textView.setText(lang.getName());
             textView.setGravity(Gravity.CENTER);
-            mLayout.addView(textView, mLayout.getChildCount() - 1);
+            mLayout.addView(textView);
         }
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        return onQueryTextChange(s);
+    }
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        s = s.replaceAll("\\s", "").replaceAll("\\+", "\\+").toUpperCase();
+        for(int i = 0; i < mLayout.getChildCount(); i++){
+            View v = mLayout.getChildAt(i);
+            if(((TextView)v).getText().toString().replaceAll("\\s", "").toUpperCase().contains(s))
+                v.setVisibility(View.VISIBLE);
+            else
+                v.setVisibility(View.GONE);
+        }
+        return false;
     }
 }
